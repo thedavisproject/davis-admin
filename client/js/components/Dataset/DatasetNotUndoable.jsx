33201f00bfb8ef4b2b3716bf583670746f45fd52
Mike Lambert <michael.lambert@velir.com>
2017-08-04 16:47:54 -0400

    Mutation of dataset

--- client/js/components/Dataset/DatasetNotUndoable.jsx
+++ client/js/components/Dataset/DatasetNotUndoable.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 
 import { func, number, shape, string }   from "prop-types";
-import { historyType }   from "../Form/Undoable.jsx";
 
 
 const propTypes = {
   dataset: shape({
     id: number.isRequired,
     name: string.isRequired
-  }).isRequired
+  }).isRequired,
+  onDatasetUpdate: func.isRequired
 };
 
 const Dataset = (props) => {
 
-  const { dataset } = props;
+  const { dataset, onDatasetUpdate } = props;
+
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+
+    onDatasetUpdate({ name: value });
+  };
 
   return (
     <div className="dataset">
 
       <h2>{dataset.name}</h2>
+
+      <input type="text" value={dataset.name} onChange={handleNameChange} />
 
       <pre>
         {JSON.stringify(props, null, 2)}
--- client/js/components/Dataset/DatasetWithData.js
+++ client/js/components/Dataset/DatasetWithData.js
@@ -1,42 +1,75 @@
 import R from "ramda";
 import { graphql, gql } from "react-apollo";
-import Dataset from "./Dataset.jsx";
 import DatasetNotUndoable from "./DatasetNotUndoable.jsx";
 import Fetchable from "../Fetchable/Fetchable.jsx";
 
-const query = gql`query datasetById($id:Int){
-  entities {
-    dataSet(id: $id) {
-      id
-      name
-    }
-  }
-}`;
-
-
-const mapResultsToProps = ({ ownProps, data }) => {
-  return {
-    dataset: R.defaultTo([], R.path(["entities","dataSet", 0], data)),
-    loading: data.loading,
-    error: data.error
-  };
-};
-
-
-const mapPropsToOptions = (props) => {
-
-  const { id } = props;
-
-  return {
-    variables: { id }
-  };
-};
-
 
 export default R.compose(
-  graphql(query, {
-    props: mapResultsToProps,
-    options: mapPropsToOptions
+
+  // load the data
+  graphql(gql`
+    query datasetById($id: Int){
+      entities {
+        dataSet(id: $id) {
+          id
+          name
+        }
+      }
+    }
+  `, {
+    props: ({ ownProps, data }) => ({
+      dataset: R.defaultTo([], R.path(["entities","dataSet", 0], data)),
+      loading: data.loading,
+      error: data.error
+    }),
+    options: (props) => ({
+      variables: {
+        id: props.id
+      }
+    })
   }),
+
+  // mutation to update the dataset
+  graphql(gql`
+    mutation updateDataset($entity: EntityUpdate) {
+      entities {
+        update(entities: [$entity]) {
+          id
+          name
+        }
+      }
+    }
+  `, {
+    props: ({ ownProps, mutate }) => ({
+
+      /**
+       * @param {Object} fields key/values of fields of the dataset to update
+       * @returns {Nothing} will mutate the dataset in graphql
+       */
+      onDatasetUpdate: (fields) => {
+
+        // id and entityType are required to select the correct entity
+        // combine them the given fields
+        const variables = {
+          entity: R.merge(fields, {
+            id: ownProps.id,
+            entityType: "dataset"
+          })
+        };
+
+        // update!
+        mutate({ variables })
+          // .then(data => {
+          //   console.log("data", data);
+          // })
+          .catch(error => {
+            console.log("error", error);
+          });
+      }
+    })
+  }),
+
+  // wrap this component in Fetchable
   Fetchable
+
 )(DatasetNotUndoable);
